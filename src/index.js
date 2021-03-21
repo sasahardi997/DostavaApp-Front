1a843fab1e75f05727f2cd40ce54002e0e9aaaeb
sasahardi997 <sasahardi@gmail.com>
2021-03-21 19:02:47 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,65 @@
 import React from 'react';
+import { Button, Container, Nav, Navbar } from 'react-bootstrap';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {HashRouter as Router, Route, Switch, Link, Redirect} from 'react-router-dom';
+import Login from './components/authorization/Login';
+import DodajNarudzbinu from './components/Narudzbine/DodajNarudzbinu';
+import Narudzbine from './components/Narudzbine/Narudzbine';
+import Racun from './components/Narudzbine/Racun';
+import Home from './Home';
+import NotFound from './NotFound';
+import {logout} from './services/auth';
+
+class App extends React.Component {
+
+    render() {
+
+        const jwt = window.localStorage['jwt'];
+
+        if(jwt){
+            return (    
+                <div>
+                    <Router>
+                        <Navbar expand bg="dark" variant="dark">
+                            <Navbar.Brand as={Link} to="/">
+                                JWD
+                            </Navbar.Brand>
+                            <Nav>
+                                <Nav.Link as={Link} to="/narudzbine">
+                                    Narudzbine
+                                </Nav.Link>
+                                <Button style={{right: '10px', position: 'absolute'}} onClick={() => logout()}>Logout</Button>
+                            </Nav>
+                        </Navbar>
+                        <Container style={{paddingTop: "25px"}}>
+                            <Switch>
+                                <Route exact path="/" component = {Home} />
+                                <Route exact path="/login" component = {Login} />
+                                <Route exact path="/narudzbine" component = {Narudzbine}/>
+                                <Route exact path="/narudzbine/dodaj" component = {DodajNarudzbinu}/>
+                                <Route exact path="/narudzbine/racun/:id" component = {Racun} />
+                                <Route component = {NotFound} />
+                            </Switch>
+                        </Container>
+                    </Router>
+                </div>
+             );
+         } else {
+            return (
+                <Container>
+                    <Router>   
+                        <Switch>
+                            <Route exact path="/login" component = {Login} />
+                            <Route render={() => <Redirect to ="/login"/>}/>
+                        </Switch>
+                    </Router>
+                </Container>
+             );
+         }
+    }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App/>,
+    document.querySelector("#root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
